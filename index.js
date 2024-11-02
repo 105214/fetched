de96876cd69a5175b2bb083c4443655d85edb39a
Nithin sukumar <nithinsukumar200@gmail.com>
2024-11-03 00:02:12 +0530

    fetch api

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-alert("working")
+fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>{ 
+
+let productBox=document.getElementById('product-box')
+let product=""
+for(let products of json){
+product=+`<div class="product-box col-md-3" id="product-box">
+<div class="card">
+  <img src="${json.image}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>`
+}
+productBox.innerHTML=product
+            })
+
+            
