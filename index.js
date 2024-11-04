c726760d426ffb6c34e596e40bd66429643acfbb
Nithin sukumar <nithinsukumar200@gmail.com>
2024-11-04 22:51:31 +0530

    completed

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// fetch('https://fakestoreapi.com/products')
+//             .then(res=>res.json())
+//             .then(json=>{ 
+// console.log(json.title)
+
 fetch('https://fakestoreapi.com/products')
             .then(res=>res.json())
             .then(json=>{ 
@@ -5,18 +10,19 @@ fetch('https://fakestoreapi.com/products')
 let productBox=document.getElementById('product-box')
 let product=""
 for(let products of json){
-product=+`<div class="product-box col-md-3" id="product-box">
+product += `<div class="col-md-3 col-sm-12 col-lg-4">
 <div class="card">
-  <img src="${json.image}" class="card-img-top" alt="...">
+  <img src="${products.image}" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <h5 class="card-title">"${products.title}"</h5>
+    <p class="card-text">"${products.price}"</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
-</div>`
+</div>
+`
 }
 productBox.innerHTML=product
-            })
+           })
 
             
